0d4488fd4721e78a04ae007003d5e1e4ed2cfa69
Alexandre Roy <alexandre15roy@gmail.com>
2024-09-25 12:35:04 -0400

    Création Enum

---
+++ 420-14B-FX-A24-TP1/enums/TypeChambre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420_14B_FX_A24_TP1.enums
+{
+    enum TypeChambre
+    {
+        Simple,
+        Double,
+        Suite,
+        Deluxe,
+        Familiale
+    }
+}
