a63dc9ae4855c476f75b6f925d3ea27626037c43
Alexandre Roy <alexandre15roy@gmail.com>
2024-09-25 11:08:02 -0400

    Ajoutez des fichiers projet.

---
+++ 420-14B-FX-A24-TP1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _420_14B_FX_A23_TP1.classes;
+using _420_14B_FX_A24_TP1.classes;
+
+namespace _420_14B_FX_A24_TP1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+
+
+        }
+
+ 
+        private void btnEffacerRecherche_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void btnRechercheChambre_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lstChambres_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEffacerReservation_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+
+        private void btnCreerReservation_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void btnEffacerRechercheReservation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRechercherReservation_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSupprimerReservation_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+    }
+}
---
+++ 420-14B-FX-A24-TP1/classes/Chambre.cs
@@ -0,0 +1,61 @@
+﻿
+
+namespace _420_14B_FX_A24_TP1.classes
+{
+    /// <summary>
+    /// Représente une chambre dans le système de gestion d'un hôtel.
+    /// </summary>
+    public class Chambre
+    {
+        // Champs privés
+        private ushort _numero;
+        private decimal _prixParNuit;
+
+        /// <summary>
+        /// Obtient ou définit le numéro de la chambre.
+        /// </summary>
+        public ushort Numero
+        {
+            get { return _numero; }
+            set { 
+                //Todo : Ajouter le formattage
+                _numero = value; 
+            }
+        }
+
+        /// <summary>
+        /// Obtient ou définit le type de la chambre (par exemple, Simple, Double, Suite).
+        /// </summary>
+        //Todo : Ajouter la propriété manquante
+
+        /// <summary>
+        /// Obtient ou définit le prix par nuit de la chambre.
+        /// </summary>
+        public decimal PrixParNuit
+        {
+            get { return _prixParNuit; }
+            set { _prixParNuit = value; }
+        }
+
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe Chambre/>.
+        /// </summary>
+       
+        
+        //Todo : Implémenter le constructeur avec paramèttres
+
+
+        /// <summary>
+        /// Retourne une chaîne de caractères représentant l'objet Chambre.
+        /// </summary>
+        /// <returns>
+        /// Une chaîne de caractères représentant les informations de la chambre.
+        /// </returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
